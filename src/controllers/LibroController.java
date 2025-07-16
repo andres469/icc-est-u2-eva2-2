fdd7ba467742ad05a9ee2e4ff4dc4e60171597cf
Andres Tenesaca <jtenesaca74@gmail.com>
2025-07-16 16:33:04 -0500

    First Commit

--- src/controllers/LibroController.java
+++ src/controllers/LibroController.java
@@ -4,11 +4,5 @@ import models.Book;
 import java.util.*;
 
 public class LibroController {
-    public Map<Book, Book> procesarLibros(List<Book> libros) {
-        Map<Book, Book> resultado = new TreeMap<>();
-        for (Book libro : libros) {
-            resultado.putIfAbsent(libro, libro); // Evita duplicados según equals/hashCode
-        }
-        return resultado;
-    }
+    
 }
